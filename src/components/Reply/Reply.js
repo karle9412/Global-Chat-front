9d17f9143fec3b97a1382a013b82c5789933c216
karle9412 <rhlanf7gh53@gmail.com>
2023-01-13 18:54:05 +0900

    게시판 merge

--- src/components/Reply/Reply.js
+++ src/components/Reply/Reply.js
@@ -3,14 +3,9 @@ import React, { useState } from "react";
 import usefetch from "../../hooks/usefetch";
 
 // 댓글은 유저이메일과 보드넘버가 필요
-export default function Reply() {
+export default function Reply(bno) {
 
-
-  const bno = '1';
-  const reply = usefetch(`http://localhost:8080/reply/request/${bno}`)
-  if (reply.length != 0) {
-    console.log(reply);
-  }
+  const reply = usefetch(`http://localhost:8080/reply/request/${bno.bno}`)
 
   //File id는 유저이메일에 들어있는 파일 기본값을 이용해서 들고 오기
   const id = '4028b28e85a9efd20185a9f4dbaa0001'
