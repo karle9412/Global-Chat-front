41df19ba7a45b016f2052097998c9f961d487c9e
karle9412 <rhlanf7gh53@gmail.com>
2023-01-16 18:49:44 +0900

    댓글 사진 달기, 및 아이디 찾기, 비밀번호 찾기 등등 수정

--- src/components/login/FindId.js
+++ src/components/login/FindId.js
@@ -1,30 +1,55 @@
-import React, { useEffect } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './FindId.css';
+import PhoneNumber from '../Registry/PhoneNumber';
 
 export default function FindId() {
 
-    const [inputValue, setInputValue] = React.useState('');
+    const [inputValue, setInputValue] = useState('');
+    const [email,setEmail] =  useState('');
+
+    
     const handleChange = (e) => {
-        const regex = /^[0-9\b -]{0,13}$/;
-        if(regex.test(e.target.value)){
-            setInputValue(e.target.value);
-        }
+            setInputValue(e);
     }
 
     useEffect(() => {
         if(inputValue.length === 10){
             setInputValue(inputValue.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3'));
+            // setInput(inputValue);
             console.log(inputValue);
+            // console.log(input);
         }
         if(inputValue.length === 13){
             setInputValue(inputValue.replace(/-/g, '').replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3'));
+            // setInput(inputValue);
             console.log(inputValue);
+            // console.log(input);
         }
     
     }, [inputValue]);
 
+    const findemail = () => {
+        axios.post('auth/getemail', 
+    {
+        phonenumber: inputValue.replace(/-/g, '')
+    }
+    )
+    .then(function (response) {
+        console.log(response)
+        setEmail(response.data.email);
+    })
+    .catch(function (error) {
+        console.log(error)
+        console.log(inputValue.replace(/-/g, ''))
+        alert("핸드폰 번호를 다시 확인해 주세요.");
+    })}
 
+    
+
+    
+    
     return (
         <>
             <div className='findIdDiv'>
@@ -35,24 +60,29 @@ export default function FindId() {
                      <hr className='hr'/>
                     
                         <div className='boxDiv'>
-                            <input className='telBox' type="text" 
-                            placeholder='휴대폰 번호를 입력해주세요.' onChange={handleChange} value={inputValue}></input>
-                            <button className='authBtn' type='button'>인증번호</button>
+                            <PhoneNumber 
+                            value= {inputValue}
+                            onChange={(value) => handleChange(value)}/>
+                        
+                            {/* <input className='telBox' type="text" 
+                            placeholder='휴대폰 번호를 입력해주세요.' onChange={handleChange} value={inputValue}></input> */}
+                            <button onClick={findemail}>
+                                아이디 찾기
+                            </button>
+                            {/* <button className='authBtn' type='button'>확인</button> */}
                         </div>
 
-                        <div className='boxDiv2'>
-                            <input className='authBox' type="text"
-                            placeholder='인증번호를 입력해주세요.'></input>
-                            <button className='authBtn2' type='button'>인증확인</button>
+                        <div>
+                           {email}
                         </div>
 
                         <div className='boxDiv3'>
-                            <Link to='/findPw'>비밀번호를 잊어버리셨나요? 여기를 클릭하세요!</Link>
+                            <Link to='/findPw'>비밀번호 찾기</Link>
                         </div>
                     </div>
             </div>
 
         </>
     );
-}
+    }
 
